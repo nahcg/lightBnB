b524cf963872adbe3d8bc2d5389a3bb24f5070da
nahcg <glor.chan@mail.utoronto.ca>
2023-07-16 11:48:58 -0400

    refactor getAllReservations function to get real reservations assocaited with user from db

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -94,7 +94,22 @@ const addUser = (user) => {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  //query lightbnb database for reservations associated with a specific user
+    .query(`SELECT reservations.*, properties.*
+    FROM reservations JOIN properties ON properties.id = reservations.property_id 
+    JOIN property_reviews ON property_reviews.property_id = properties.id 
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id 
+    ORDER BY reservations.start_date 
+    LIMIT $2;`, [guest_id, limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /// Properties
