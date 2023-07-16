e334bb1bf09f8b4b13272a4bb867747b51b783f8
nahcg <glor.chan@mail.utoronto.ca>
2023-07-16 18:21:37 -0400

    refactor functions to consider WHERE and HAVING clauses for filters

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -122,8 +122,99 @@ const getAllReservations = function (guest_id, limit = 10) {
  */
 
 const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+  //array to hold any parameters that may be available for the query
+  const queryParams = [];
+  //query with all information that comes before the WHERE clause
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) AS average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // check if a city has been passed in as an option
+  // Add the city to the params array and create a WHERE clause for the city
+  if (options.city) {
+    if (queryParams.length === 0) {
+      queryParams.push(`%${options.city}%`);
+      queryString += `WHERE `;
+      queryString += `city LIKE $${queryParams.length} `;
+    } else {
+      queryParams.push(`%${options.city}%`);
+      queryString += `AND city LIKE $${queryParams.length} `;
+    }
+  }
+
+  // only return properties belonging to that owner
+  if (options.owner_id) {
+    if (queryParams.length === 0) {
+      queryParams.push(`${options.owner_id}`);
+      queryString += `WHERE `;
+      queryString += `properties.owner_id = $${queryParams.length} `;
+    } else {
+      queryParams.push(`${options.owner_id}`);
+      queryString += `AND properties.owner_id = $${queryParams.length} `;
+    }
+  };
+
+  // only return properties above min price
+  if (options.minimum_price_per_night) {
+    console.log(queryParams.length)
+    if (queryParams.length === 0) {
+      queryParams.push(`${options.minimum_price_per_night * 100}`);
+      queryString += `WHERE `;
+      queryString += `properties.cost_per_night > $${queryParams.length} `;
+    } else {
+      queryParams.push(`${options.minimum_price_per_night * 100}`);
+      queryString += `AND properties.cost_per_night > $${queryParams.length} `;
+    }
+  }
+
+   // only return properties below max price
+   if (options.maximum_price_per_night) {
+    console.log(queryParams.length)
+    if (queryParams.length === 0) {
+      queryParams.push(`${options.maximum_price_per_night * 100}`);
+      queryString += `WHERE `;
+      queryString += `properties.cost_per_night < $${queryParams.length} `;
+    } else {
+      queryParams.push(`${options.maximum_price_per_night * 100}`);
+      queryString += `AND properties.cost_per_night < $${queryParams.length} `;
+    }
+  }
+
+  // only return properties with an average rating equal to or higher than rating as HAVING clause after the GROUP BY
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `
+    GROUP BY properties.id 
+    HAVING avg(property_reviews.rating) >= $${queryParams.length}`;
+  };
+
+
+  // add query that comes after the WHERE clause
+  // if no having clause after group by
+  if (!options.minimum_rating) {
+    queryParams.push(limit);
+    queryString += `
+    GROUP BY properties.id
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+  // if there is a having clause
+  } else if (options.minimum_rating) {
+    queryParams.push(limit);
+    queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+  };
+  
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams)
     .then((result) => {
       console.log(result.rows);
       return result.rows;
@@ -132,7 +223,6 @@ const getAllProperties = (options, limit = 10) => {
       console.log(err.message);
     });
 };
-
 
 
 /**
